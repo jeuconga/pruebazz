db4eab9230502b6cd27aec978509cc88a54ce4e7
Usuario <Usuario@DESKTOP-35SEOO3.mshome.net>
2021-09-28 10:06:55 +0200

    Merge origin/master
    Conflicts:
    	HiberLibros/src/main/java/com/hiberlibros/HiberLibros/controllers/InicioController.java

--- src/main/java/com/heroku/pruebaZZ/entities/Persona.java
+++ src/main/java/com/heroku/pruebaZZ/entities/Persona.java
@@ -8,11 +8,12 @@ package com.heroku.pruebaZZ.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 /**
  *
@@ -24,9 +25,11 @@ import org.springframework.data.annotation.Id;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Persona {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
     private String nombre;
     
 }
